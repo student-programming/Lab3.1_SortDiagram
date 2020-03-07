2b1c4adb2b8de38fbccc96ca2c244eff7c25db5c
Класс <class@vyatsu.ru>
2020-03-07 10:34:51 +0300

    The Laba

---
+++ SortDiagram/SortDiagram/BubbleSorter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SortDiagram
+{
+    class BubbleSorter
+    {
+        private int _i;
+        private int _j;
+        private readonly int[] _numbers;
+
+        public BubbleSorter(int[] numbers)
+        {
+            _numbers = numbers;
+        }
+
+        public int[] Sort(CheckBox _chbInvers)
+        {
+            if (_chbInvers.Checked == false)
+            {
+                Array.Sort(_numbers, 0, _numbers.Length);
+            }
+            else
+            {
+                Array.Sort(_numbers, 0, _numbers.Length);
+                Array.Reverse(_numbers);
+            }
+            return _numbers;
+        }
+        public int[] StepByStepSort()
+        {
+            bool exit = false;
+            for (int i = _i; i < _numbers.Length; i++)
+            {
+                for (int j = _j; j < _numbers.Length - 1 - i; j++)
+                {
+                    if(_numbers[j] > _numbers[j + 1])
+                    {
+                        int temp = _numbers[j];
+                        _numbers[j] = _numbers[j + 1];
+                        _numbers[j + 1] = temp;
+                        _i = i;
+                        _j = j;
+                        exit = true;
+                    }
+                    if (j >= _numbers.Length - 2 - i)
+                        _i++;
+                    if (exit)
+                        break;
+                }
+                if (_i > i) _j = 0;
+                if (exit)
+                    break;
+            }
+
+            return _numbers;
+        }
+    }
+}
---
+++ SortDiagram/SortDiagram/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SortDiagram
+{
+    public partial class Form1 : Form
+    {
+        private readonly GraphPainter _painter;
+        private int[] _datacolumns;
+        private BubbleSorter _sorter;
+        public Form1()
+        {
+            InitializeComponent();
+            _painter = new GraphPainter(chGraph);
+        }
+        private void btnGenerateNums_Click_1(object sender, EventArgs e)
+        {
+            _datacolumns = new int[Convert.ToInt32(tbCountNumbers.Text)];
+            tbNums.Text = " ";
+            Random random = new Random();
+            for (int i = 0; i < _datacolumns.Length; i++)
+            {
+                int num = random.Next(0, 99);
+                _datacolumns[i] = num;
+                tbNums.Text += num + " ";
+            }
+            tbNums.Text = tbNums.Text.Trim();
+            _painter.PaintGraph(_datacolumns);
+            _sorter = new BubbleSorter(_datacolumns);
+        }
+
+        private void btnSort_Click_1(object sender, EventArgs e)
+        {
+            if (tbNums.Text.Length == 0 || _sorter == null) return;
+            _datacolumns = _sorter.Sort(chbInvers);
+            _painter.PaintGraph(_datacolumns);
+        }
+
+        private void btnNextStep_Click_1(object sender, EventArgs e)
+        {
+            if (tbNums.Text.Length == 0 || _sorter == null) return;
+            _datacolumns = _sorter.StepByStepSort();
+            _painter.PaintGraph(_datacolumns);
+        }
+    }
+}
