ff93325c6ad8c45bc4b3bae1dcfe720947d50219
Класс <class@vyatsu.ru>
2020-03-14 10:39:40 +0300

    done

---
+++ SortDiagram.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chGraph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbCountNumbers = new System.Windows.Forms.TextBox();
+            this.tbNums = new System.Windows.Forms.TextBox();
+            this.btnGenerateNums = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnNextStep = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chGraph)).BeginInit();
+            this.SuspendLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chGraph;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbCountNumbers;
+        private System.Windows.Forms.TextBox tbNums;
+        private System.Windows.Forms.Button btnGenerateNums;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnNextStep;
+    }
+}
+
---
+++ SortDiagram/SortDiagram/GraphPainter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SortDiagram
+{
+    class GraphPainter
+    {
+        private readonly Chart _chart;
+        public GraphPainter(Chart chart)
+        {
+            _chart = chart;
+        }
+        public void PaintGraph(int[] nums)
+        {
+            _chart.Series[0].Points.Clear();
+            foreach (int num in nums)
+            {
+                _chart.Series[0].Points.Add(num);
+                int count = _chart.Series[0].Points.Count;
+                _chart.Series[0].Points[--count].Color = ColorPoint(num);
+            }
+        }
+        private Color ColorPoint(int num)
+        {
+            if (num < 7) return Color.Yellow;
+            if (num < 14) return Color.Blue;
+            if (num < 21) return Color.Green;
+            if (num < 28) return Color.LightBlue;
+            if (num < 35) return Color.MediumPurple;
+            if (num < 42) return Color.AntiqueWhite;
+            if (num < 49) return Color.Aquamarine;
+            if (num < 56) return Color.Bisque;
+            if (num < 63) return Color.Indigo;
+            if (num < 70) return Color.Brown;
+            if (num < 77) return Color.Coral;
+            if (num < 84) return Color.DarkBlue;
+            if (num < 91) return Color.DarkGoldenrod;
+            return Color.LightPink;
+        }
+    }
+}
